4d772c6771b92ff0552a50c77827d7cff190494d
LipatovAlexander <lipatov.work@bk.ru>
2023-04-23 16:33:20 +0300

    fix: Entities configurations

---
+++ backend/Infrastructure/EntitiesConfigurations/MaterialConfig.cs
@@ -0,0 +1,16 @@
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.EntitiesConfigurations;
+
+public sealed class MaterialConfig : IEntityTypeConfiguration<Material>
+{
+    public void Configure(EntityTypeBuilder<Material> builder)
+    {
+        builder.ToTable("Material");
+
+        builder.Property(m => m.MaterialType)
+            .HasConversion<string>();
+    }
+}
