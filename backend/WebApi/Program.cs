ce02e8b5fd5f117114ad20c4d0f4e1227f6a6b97
LipatovAlexander <lipatov.work@bk.ru>
2023-04-23 17:53:49 +0300

    feat: Add swagger support

--- backend/WebApi/Program.cs
+++ backend/WebApi/Program.cs
@@ -1,12 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 services.AddValidators();
 services.AddEndpoints();
 
 services.AddApplicationDbContext();
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapEndpoints()
     .AddValidationFilter();
