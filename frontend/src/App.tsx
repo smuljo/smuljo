72d81e82fd9c438d65acb625fd45d59855bf1259
ArtemKhamzin <86554484+ArtemKhamzin@users.noreply.github.com>
2023-05-18 00:33:10 +0300

    edit App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,9 @@
 import {BrowserRouter} from "react-router-dom";
 import './App.css'
-import {useEffect, useState} from "react";
 import AppRoutes from "./routes/AppRoutes";
 import Navbar from "./components/Navbar";
 
 function App() {
-    const [isAuth, setIsAuth] = useState(false);
-
-    useEffect(() => {
-        if(localStorage.getItem('auth')){
-            setIsAuth(true)
-        }
-    }, [])
-
     return (
         <BrowserRouter>
             <Navbar/>
